c51c9ab0fc3dceb87de7fd7f89d9ba2ee5ad393d
Aiden Rougeot <aiden.rougeot@gmail.com>
2022-09-26 19:45:22 -0700

    New Student and Update Status
    Modified application.properties to work with local mysql database
    Added StudentDTO
    Added StudentController
    	Added addStudent method
    	Added updateStatus method
    Added JunitTestStudent
    	Added tests for addStudent and updateStatus methods

---
+++ cst438_register/src/main/java/com/cst438/domain/StudentDTO.java
@@ -0,0 +1,13 @@
+package com.cst438.domain;
+
+public class StudentDTO {
+	public int student_id;
+	public String name;
+	public String email;
+	public int statusCode;
+	public String status;
+	
+	public StudentDTO() {
+		super();
+	}
+}
